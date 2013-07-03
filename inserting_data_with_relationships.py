14078ea0bf0a8b91a1b0cf2828c21fdcdb4b75ba
Adam McNicol <adamsorders@mac.com>
2013-07-03 15:26:53 +0100

    initial related table insert
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

---
+++ inserting_data_with_relationships.py
@@ -0,0 +1,20 @@
+#03-07-2013
+#inserting data into tables with reference to other tables
+
+from database_functions import query
+
+def insert_product_data(records):
+    sql = "insert into product (name,price,product_type_id) values (?,?,?)"
+    for record in records:
+        query(sql, record)
+
+def insert_product_type_data(records):
+    sql = "insert into product_type(name) values (?)"
+    for record in records:
+        query(sql, record)
+
+if __name__ == "__main__":
+    products = [("Americano",2.0,1),("Mocha",3.5,1),("Green Tea with Lemon",1.25,2)]
+    #insert_product_data(products)
+    product_types = [("Coffee",),("Tea",),("Cold Drink",)]
+    insert_product_type_data(product_types)
