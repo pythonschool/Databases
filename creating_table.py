026754a26eddebf81caebf0e1ff942119957d054
Adam McNicol <adamsorders@mac.com>
2013-07-03 10:12:48 +0100

    improved create table syntax to check for existing table
    Signed-off-by: Adam McNicol <adamsorders@mac.com>

--- creating_table.py
+++ creating_table.py
@@ -3,12 +3,27 @@
 
 import sqlite3
 
-def create_table(db_name,sql):
+def create_table(db_name,table_name,sql):
     with sqlite3.connect(db_name) as db:
+        print(table_name)
         cursor = db.cursor()
-        cursor.execute(sql)
-        db.commit()
-
+        cursor.execute("select name from sqlite_master where name=?",(table_name,))
+        result = cursor.fetchall()
+        keep_table = True
+        if len(result) == 1:
+            response = input("The table {0} already exists, do you wish to recreate it? (y/n): ".format(table_name))
+            if response == "y":
+                keep_table = False
+                print("The {0} table will be recreated - all existing data will be lost".format(table_name))
+                cursor.execute("drop table if exists {0}".format(table_name))
+                db.commit()
+            else:
+                print("The existing animal table was kept")
+        else:
+            keep_table = False
+        if not keep_table:
+            cursor.execute(sql)
+            db.commit()
 
 if __name__ == "__main__":
     db_name = "coffee_shop.db"
@@ -17,5 +32,5 @@ if __name__ == "__main__":
             name text,
             price real,
             primary key(product_id))"""
-    create_table(db_name, sql)
+    create_table(db_name, "product", sql)
 
